95c7d56ddeb6a4bd779e830f3941ff9349e83b52
Subasish Mohapatra <subasish.mohapatra@hotmail.com>
2021-04-14 16:01:25 +0530

    Add project files.

---
+++ AzureServiceBusReceiver/Program.cs
@@ -0,0 +1,65 @@
+﻿using Azure.Messaging.ServiceBus;
+using System;
+using System.Threading.Tasks;
+
+namespace AzureServiceBusReceiver
+{
+    class Program
+    {
+        static string connectionString = "ServiceBusNamespaceConnectionString";
+        static string queueName = "test";
+
+        static async Task Main()
+        {
+            await ReceiveMessagesAsync();
+        }
+
+        static async Task ReceiveMessagesAsync()
+        {
+            await using (ServiceBusClient client = new ServiceBusClient(connectionString))
+            {
+                // create a processor that we can use to process the messages
+                ServiceBusProcessor processor = client.CreateProcessor(queueName, new ServiceBusProcessorOptions());
+
+                // add handler to process messages
+                processor.ProcessMessageAsync += MessageHandler;
+
+                // add handler to process any errors
+                processor.ProcessErrorAsync += ErrorHandler;
+
+                // start processing 
+                await processor.StartProcessingAsync();
+
+                Console.WriteLine("Press escape to exit processing messages");
+                while (1==1) {
+                    var input = Console.ReadKey();
+                    if (input.Key == ConsoleKey.Escape)
+                    {
+                        break;
+                    }
+                }
+                // stop processing 
+                Console.WriteLine("Stopping the receiver...");
+                await processor.StopProcessingAsync();
+                Console.WriteLine("Stopped receiving messages");
+            }
+        }
+
+        // handle received messages
+        static async Task MessageHandler(ProcessMessageEventArgs args)
+        {
+            string body = args.Message.Body.ToString();
+            Console.WriteLine($"Received: {body}");
+
+            // complete the message. messages is deleted from the queue. 
+            await args.CompleteMessageAsync(args.Message);
+        }
+
+        // handle any errors when receiving messages
+        static Task ErrorHandler(ProcessErrorEventArgs args)
+        {
+            Console.WriteLine(args.Exception.ToString());
+            return Task.CompletedTask;
+        }
+    }
+}
---
+++ AzureServiceBusSender/Program.cs
@@ -0,0 +1,40 @@
+﻿using Azure.Messaging.ServiceBus;
+using System;
+using System.Threading.Tasks;
+
+namespace AzureServiceBusSender
+{
+    class Program
+    {
+        static string connectionString = "ServiceBusNamespaceConnectionString";
+        static string queueName = "test";
+
+        static async Task Main()
+        {
+            Console.WriteLine("Message sender started...");
+            await SendMessageAsync();
+        }
+
+        static async Task SendMessageAsync()
+        {
+            var ctr = 1;
+            // create a Service Bus client 
+            await using (ServiceBusClient client = new ServiceBusClient(connectionString))
+            {
+                // create a sender for the queue 
+                ServiceBusSender sender = client.CreateSender(queueName);
+
+                Console.WriteLine("Press escape to exit process, else any key to send next message");
+                while (!(Console.ReadKey().Key == ConsoleKey.Escape))
+                {
+                    // create a message that we can send
+                    ServiceBusMessage message = new ServiceBusMessage($"message {ctr++}");
+
+                    // send the message
+                    await sender.SendMessageAsync(message);
+                    Console.WriteLine($"Sent {message.Body} to the queue {queueName}");
+                }
+            }
+        }
+    }
+}
